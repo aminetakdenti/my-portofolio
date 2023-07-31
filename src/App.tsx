70a636880c4b42048e584f6dfff88c119a9dba1a
amine takdenti <aminetakdenti4@gmail.com>
2023-07-31 15:20:00 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="bg-blue-400  md:mx-10 lg:mx-24"></div>;
+}
+
+export default App;
---
+++ src/routes/Root.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+function Root() {
+  return (
+    <div className="container mx-auto">
+      <Header />
+    </div>
+  );
+}
+
+export default Root;
