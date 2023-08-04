653e55a48e5fbe0c9e9cef9faa5bf0f8c185d214
amine takdenti <aminetakdenti4@gmail.com>
2023-08-04 17:54:37 +0100

    fix the links file

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -5,36 +5,32 @@ import Reveal from "./Reveal";
 function Links() {
   return (
     <div className=" flex gap-4">
-      <div className="flex gap-2 ">
-        <Reveal>
-          <>
-            <GitHubIcon />
-            <a
-              href="http://github.com/aminetakdenti"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="relative after:absolute after:-bottom-[1px] after:left-0 after:h-[1px] after:w-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full"
-            >
-              github
-            </a>
-          </>
-        </Reveal>
-      </div>
-      <div className="flex gap-2 ">
-        <Reveal>
-          <>
-            <LinkedInIcon />
-            <a
-              href="https://www.linkedin.com/in/amine-takdenti-4a709b1b1/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="relative after:absolute after:-bottom-[1px] after:left-0 after:h-[1px] after:w-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full"
-            >
-              LinkedIn
-            </a>
-          </>
-        </Reveal>
-      </div>
+      <Reveal>
+        <div className="flex gap-2 ">
+          <GitHubIcon />
+          <a
+            href="http://github.com/aminetakdenti"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative after:absolute after:-bottom-[1px] after:left-0 after:h-[1px] after:w-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full"
+          >
+            github
+          </a>
+        </div>
+      </Reveal>
+      <Reveal>
+        <div className="flex gap-2 ">
+          <LinkedInIcon />
+          <a
+            href="https://www.linkedin.com/in/amine-takdenti-4a709b1b1/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative after:absolute after:-bottom-[1px] after:left-0 after:h-[1px] after:w-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </Reveal>
     </div>
   );
 }
